6b66a318897cf50c9ed66a3ff9a3e81e8f13c92a
AdamM1-36 <72456013+AdamM1-36@users.noreply.github.com>
2023-09-14 11:12:55 +0700

    replaced B | modulation

--- B.py
+++ B.py
@@ -1,61 +1,67 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 13 03:30:09 2023
+Created on Thu Sep 14 00:01:14 2023
 
 @author: adam
 """
 
-#!/bin/python
-# -*- coding: utf-8 -*-
-
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-def myFFT(v):
-    n = len(v)
+# Function to generate sine wave using params: amplitude, frequency, time axis
+def generate_sine_wave(amplitude: float, frequency: float, dt: np.ndarray):
+    return amplitude * np.sin(2 * np.pi * frequency * dt)
+
+# Assign carrier and message wave characteristics
+carrier_amplitude: float = 500
+carrier_frequency: float = 1000
+message_amplitude: float = 300
+message_frequency: float = 60
+
+# Time axis, from 0 to 0.1s with 1000 partition
+time = np.linspace(start= 0, stop= 0.1, num= 1000)
+
+# Main program
+if __name__ == '__main__':
+    # Generate carrier and message signal
+    carrier_signal = generate_sine_wave(carrier_amplitude, carrier_frequency, time)
+    message_signal = generate_sine_wave(message_amplitude, message_frequency, time)
     
-    if n==1:
-        return v
-    else:
-        # implement some recursive
-        F_even = myFFT(v[::2])
-        F_odd = myFFT(v[1::2])
-        
-        # frequency factor
-        fac = np.exp(-2j*np.pi*np.arange(n)/n)
-        
-        # build FFT array
-        F = np.concatenate([
-            F_even + fac[:int(n/2)]*F_odd,
-            F_even + fac[int(n/2):]*F_odd
-            ])
-        
-        return F
-
-# ID
-print("Nama: Adam Mahendra")
-print("NRP: 5009211069")
-
-# X array linear spacing
-X = np.arange(0,1,1.0/128)
-
-# Y sinus function
-Y = np.sin(2*np.pi*X)
-
-# create noise array at X length
-R = np.random.rand(len(X))
-
-# add noise to sine result
-Yr = Y + R
-
-# FFT all
-FY = np.abs(myFFT(Y))
-FYr = np.abs(myFFT(Yr))
-
-# plot all
-fig, ax = plt.subplots(2,2)
-ax[0,0].plot(X,Y)
-ax[0,1].plot(FY)
-ax[1,0].plot(X,Yr)
-ax[1,1].plot(FYr)
+    # Generate AM signal using modulation mathematical expression 
+    # (carrier_amplitude + message_signal)*sin(2*pi*carrier_freq*t)
+    # which means, the amplitude of carrier signal is modulated.
+    # Message signal amplitude is enveloping the carrier signal
+    modulated_signal = generate_sine_wave(
+        (carrier_amplitude + message_signal),
+        carrier_frequency,
+        time)
+    
+    # Create 3 rows plot
+    plt.subplot(3,1,1)
+    plt.plot(message_signal,color='blue')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Message signal')
+    
+    plt.subplot(3,1,2)
+    plt.plot(carrier_signal, color='red')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Carrier signal')
+    
+    plt.subplot(3,1,3)
+    plt.plot(modulated_signal, color="purple")
+    plt.ylabel('Amplitude')
+    plt.xlabel('AM signal')
+    
+    plt.rc('font', size=22)
+    # get current figure as Figure object
+    fig = plt.gcf()
+    # subplot title doesn't show when using default hspace = 0.2 (vertical space is too tight)
+    fig.subplots_adjust(hspace=0.4)
+    fig.set_size_inches(16, 10)
+    
+    # Save figure
+    fig.savefig('AM.jpg')
+    
+    print("Nama: Adam Mahendra")
+    print("NRP: 5009211069")
